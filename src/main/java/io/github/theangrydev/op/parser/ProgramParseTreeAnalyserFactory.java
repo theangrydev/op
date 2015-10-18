920421ff68c598acdc720ebf8e7bb8d485cce2ef
Liam Williams <liam.williams@zoho.com>
2015-10-18 21:35:02 +0100

    Refactoring

--- src/main/java/io/github/theangrydev/op/parser/ProgramParseTreeAnalyserFactory.java
+++ src/main/java/io/github/theangrydev/op/parser/ProgramParseTreeAnalyserFactory.java
@@ -4,23 +4,23 @@ import com.google.common.collect.Lists;
 import io.github.theangrydev.op.language.*;
 import io.github.theangrydev.op.semantics.ParseTreeAnalyser;
 import io.github.theangrydev.op.semantics.ParseTreeAnalysers;
-import io.github.theangrydev.op.semantics.ParseTreeLeafAnalyser;
 import io.github.theangrydev.opper.grammar.Grammar;
 
 import java.util.List;
 
 import static io.github.theangrydev.op.semantics.BinaryExpressionAnalyser.binaryExpression;
 import static io.github.theangrydev.op.semantics.ConsumeExpressionAnalyser.consumeExpression;
+import static io.github.theangrydev.op.semantics.ParseTreeLeafAnalyser.analyser;
 import static io.github.theangrydev.op.semantics.ParseTreeNodeAnalyser.analyser;
 
 public class ProgramParseTreeAnalyserFactory {
 	public static ParseTreeAnalyser<Program> programParseTreeAnalyser(Grammar grammar) {
-		ParseTreeAnalyser<TypeExpression> typeExpression = analyser(consumeExpression(ParseTreeLeafAnalyser.analyser(TypeExpression::of)));
+		ParseTreeAnalyser<TypeExpression> typeExpression = analyser(consumeExpression(analyser(TypeExpression::of)));
 
 		ParseTreeAnalysers<Expression> expressions = new ParseTreeAnalysers<>();
-		expressions.add(grammar.ruleByDefinition("Expression", "Integer"), analyser(consumeExpression(ParseTreeLeafAnalyser.analyser(IntegerConstant::of))));
-		expressions.add(grammar.ruleByDefinition("Expression", "Real"), analyser(consumeExpression(ParseTreeLeafAnalyser.analyser(RealConstant::of))));
-		expressions.add(grammar.ruleByDefinition("Expression", "String"), analyser(consumeExpression(ParseTreeLeafAnalyser.analyser(StringConstant::of))));
+		expressions.add(grammar.ruleByDefinition("Expression", "Integer"), analyser(consumeExpression(analyser(IntegerConstant::of))));
+		expressions.add(grammar.ruleByDefinition("Expression", "Real"), analyser(consumeExpression(analyser(RealConstant::of))));
+		expressions.add(grammar.ruleByDefinition("Expression", "String"), analyser(consumeExpression(analyser(StringConstant::of))));
 		expressions.add(grammar.ruleByDefinition("Expression", "TypeExpression"), analyser(consumeExpression(typeExpression)));
 		expressions.add(grammar.ruleByDefinition("Expression", "Expression", "+", "Expression"), analyser(binaryExpression(Addition::add, expressions)));
 
